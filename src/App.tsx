2ec1d1af1e738e037743dd31e3e51d79c80bcc07
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-15 19:48:16 +0200

    Update theme toggle functionality ,update navbar to use CSS variable and implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+const App: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
 
-function App() {
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
 
   return (
-    <>
+    <div className={isDarkMode ? 'dark' : 'light'}>
+    <Router>
+      <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
     
-    </>
+      </Routes>
+    </Router>
+    </div>
   )
 }
 
